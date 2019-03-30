7c16d6eefe0d7e7d7132cb8c01ce31b3a93b8fa5
s-light <github@s-light.eu>
2019-03-30 11:48:43 +0100

    [examples] added PocketBeagle digitalio

--- examples/pocketbeagle_digitalio.py
+++ examples/pocketbeagle_digitalio.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Example of blinking LED on BeagleBone Black
+# Example of blinking LED on PocketBeagle
 # https://www.adafruit.com/product/1876
 #
 # Wire the circuit as follows:
-# 1) connect anode (+) lead of LED to P9.12 pin
+# 1) connect anode (+) lead of LED to P1.36 pin
 # 2) connect cathode (-) lead to 1K Ohm resistor
-# 3) connect that 1K Ohm resistor to DGND (P9.1)
+# 3) connect that 1K Ohm resistor to GND (P1.22)
 
 import time
 import board
@@ -15,7 +15,7 @@ import digitalio
 
 print("hello blinky!")
 
-led = digitalio.DigitalInOut(board.P9_12)
+led = digitalio.DigitalInOut(board.P1_36)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
